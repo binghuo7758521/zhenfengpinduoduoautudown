3041fd3ca17604d80250f011e99de5b1a3f2240d
binghuo7758521 <56157187@qq.com>
2019-09-01 18:28:07 +0800

    2222

--- zidongxiazai/Form1.Designer.cs
+++ zidongxiazai/Form1.Designer.cs
@@ -36,6 +36,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -90,11 +92,30 @@
             this.textBox3.TabIndex = 5;
             this.textBox3.WordWrap = false;
             // 
+            // progressBar1
+            // 
+            this.progressBar1.AccessibleDescription = "";
+            this.progressBar1.AccessibleName = "";
+            this.progressBar1.Location = new System.Drawing.Point(33, 266);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(598, 17);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(639, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 19);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(805, 526);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
@@ -117,6 +138,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- zidongxiazai/Form1.cs
+++ zidongxiazai/Form1.cs
@@ -16,6 +16,8 @@ namespace zidongxiazai
 
     {
         string downdir;
+        int sumnum=0;
+        int lianxucuo = 0;//连续失败次数
         public Form1()
         {
             InitializeComponent();
@@ -31,7 +33,7 @@ namespace zidongxiazai
             downdir = ConfigurationManager.AppSettings["downdir"];
             if (downdir==null)
             {
-                downdir = "e:\\autodown\\";
+                downdir = "d:\\autodown\\";
             }
             if (!Directory.Exists(downdir))
             {
@@ -53,6 +55,7 @@ namespace zidongxiazai
             strHTML = sr.ReadToEnd();
 
             myStream.Close();
+            myWebClient.Dispose();
 
             return strHTML;
 
@@ -65,26 +68,62 @@ namespace zidongxiazai
 
         }
 
+        void client_downok(object sender, AsyncCompletedEventArgs e)
+        {
+            timer1.Enabled = true;
+            this.progressBar1.Value = 0;
+            sumnum++;
+            string aa = sumnum.ToString();
+            textBox2.AppendText(aa + "下载完成." + "\r\n");
+        }
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.progressBar1.Minimum = 0;
+            this.progressBar1.Maximum = (int)e.TotalBytesToReceive;
+            this.progressBar1.Value = (int)e.BytesReceived;
+            long i= e.TotalBytesToReceive / 1024 / 1024; 
+
+            this.label2.Text =i.ToString()+"M"; 
+             
+        }
         private void timer1_Tick(object sender, EventArgs e)
         {
             string htmtxt="";
             
             string myfilename = "";
            
-            WebClient client = new WebClient();
+          
+            
             
             timer1.Enabled = false;
             textBox2.AppendText("1.0正在读取服务器信息...." + "\r\n");
             htmtxt = GetWebClient("http://ct.ahjcg.com/auto_down.php");
-            if (htmtxt.IndexOf("无法打开文件") > 0) {
-                textBox3.AppendText("文件下载失败，请检查服务器后台...." + "\r\n");
-                textBox3.AppendText(htmtxt + "\r\n");
-                timer1.Enabled = true;
-                return;
-            }
             htmtxt = htmtxt.Replace("开始zipdownval:", "");
             int i=htmtxt.IndexOf("zip_ok:<");
             int ii=htmtxt.IndexOf(".zip>");
+            if (htmtxt.IndexOf("无法打开文件") > 0 || i<1 ||  ii<1) {
+                textBox3.AppendText("文件下载失败，请检查服务器后台...." + "\r\n");
+                textBox3.AppendText(htmtxt + "\r\n");
+                lianxucuo ++;
+
+                if (lianxucuo > 10)
+                {
+                    timer1.Enabled = false;
+                    button1.Enabled = true;
+                    textBox2.AppendText("连续出错超过10次，程序停止运行...." + "\r\n");
+
+                }
+                else
+                {
+                    timer1.Enabled = true;
+                }
+                return;
+               
+            }
+
+
+            lianxucuo = 0;
+
             string zipfile = htmtxt.Substring(i+8,ii-i-4 );
            htmtxt= htmtxt.Substring(0, htmtxt.IndexOf("zip_ok:"));
            htmtxt= htmtxt.Replace("val:","|");
@@ -95,9 +134,15 @@ namespace zidongxiazai
            string[] str2 = zipfile.Split('/');
            myfilename = str2[5];
            textBox2.AppendText("正在压缩并下载... . ." + myfilename + "\r\n");
-           client.DownloadFile(zipfile, downdir + myfilename);
-           textBox2.AppendText("下载完成." + "\r\n");
-           timer1.Enabled = true;
+           WebClient client = new WebClient();
+           client.DownloadFileCompleted += client_downok;
+           client.DownloadProgressChanged += client_DownloadProgressChanged;            
+           client.DownloadFileAsync(new Uri(zipfile), downdir + myfilename);
+           client.Dispose();
+             
+           
+           
+           //timer1.Enabled = true;
 
            
         }
